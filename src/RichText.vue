fe630bf56e42e4e6075d562a9dcb548355bd8de8
Cacodaimon <mail@cacodaemon.de>
2020-09-23 15:35:00 +0200

    Render markdown in rich text with remark & rehype.

--- src/RichText.vue
+++ src/RichText.vue
@@ -1,5 +1,6 @@
 <!--
   - @copyright Copyright (c) 2020 Julius Härtl <jus@bitgrid.net>
+  - @copyright Copyright (c) 2020 Guido Krömer <mail@cacodaemon.de>
   -
   - @author Julius Härtl <jus@bitgrid.net>
   -
@@ -21,68 +22,46 @@
   -->
 
 <script>
-import Link from './Link'
+import unified from 'unified'
+import markdown from 'remark-parse'
+import remark2rehype from 'remark-rehype'
+import rehype2react from 'rehype-react'
+import remarkDisableBlocks from 'remark-disable-tokenizers'
+import remarkExternalLinks from 'remark-external-links'
+import rehypeAddClasses from 'rehype-add-classes'
 
-const urlRegex = /(\s|\(|^)((https?:\/\/)((?:[-A-Z0-9+_]+\.)+[-A-Z]+(?::[0-9]+)?(?:\/[-A-Z0-9+&@#%?=~_|!:,.;()]*)*))(?=\s|\)|$)/ig
+/*
+* Code inspired by https://github.com/cool-cousin/vue-remark
+* */
+function pluginComponent() {
+	const { inlineTokenizers, inlineMethods } = this.Parser.prototype
+	inlineTokenizers.component = (eat, value, silent) => {
+		const match = /^\{([-\w]+)\}/.exec(value)
 
-const parseUrl = (text, linkComponent) => {
-	let match = urlRegex.exec(text)
-	const list = []
-	let start = 0
-	while (match !== null) {
-		let href = match[2]
-		let textAfter
-		let textBefore = text.substring(start, match.index + match[1].length)
-		if (href[0] === ' ') {
-			textBefore += href[0]
-			href = href.substring(1).trim()
-		}
-		const lastChar = href[(href.length - 1)]
-		if (lastChar === '.' || lastChar === ',' || lastChar === ';' || (match[0][0] === '(' && lastChar === ')')) {
-			href = href.substring(0, href.length - 1)
-			textAfter = lastChar
+		if (!match) {
+			return
 		}
 
-		list.push(textBefore)
-		list.push({ component: linkComponent, props: { href } })
-		if (textAfter) {
-			list.push(textAfter)
+		if (silent) {
+			return true
 		}
-		start = match.index + match[0].length
-		match = urlRegex.exec(text)
-	}
-	list.push(text.substring(start))
 
-	const joinedText = list.map((item) => typeof item === 'string' ? item : item.props.href).join('')
-	if (text === joinedText) {
-		return list
-	}
-	console.error('Failed to reassemble the chunked text: ' + text)
-	return text
-}
+		const [all, component] = match
 
-const prepareTextNode = ({ h, context }, text) => {
-	if (context.props.autolink) {
-		text = parseUrl(text, context.props.linkComponent)
-	}
-	if (Array.isArray(text)) {
-		return text.map((entry) => {
-			if (typeof entry === 'string') {
-				return entry
-			}
-			const { component, props } = entry
-			return h(component, {
-				props,
-				class: 'rich-text--component'
-			})
+		return eat(all)({
+			type: 'component',
+			component: `#${component}`,
+			value: ''
 		})
 	}
-	return text
+	inlineTokenizers.component.locator = (value, fromIndex) => {
+		return value.indexOf('{', fromIndex)
+	}
+	inlineMethods.splice(inlineMethods.indexOf('text'), 0, 'component')
 }
 
 export default {
 	name: 'RichText',
-	functional: true,
 	props: {
 		text: {
 			type: String,
@@ -94,10 +73,38 @@ export default {
 				return {}
 			}
 		},
-		linkComponent: {
+		markdownCssClasses: {
 			type: Object,
 			default: () => {
-				return Link
+				return {
+					a: 'rich-text--external-link',
+					li: 'rich-text--list-element',
+					strong: 'rich-text--strong',
+					em: 'rich-text--italic',
+					h1: 'rich-text--heading rich-text--heading-1',
+					h2: 'rich-text--heading rich-text--heading-2',
+					h3: 'rich-text--heading rich-text--heading-3',
+					h4: 'rich-text--heading rich-text--heading-4',
+					h5: 'rich-text--heading rich-text--heading-5',
+					h6: 'rich-text--heading rich-text--heading-6',
+					hr: 'rich-text--hr',
+					table: 'rich-text--table',
+					pre: 'rich-text--pre',
+					code: 'rich-text--code',
+					blockquote: 'rich-text--blockquote'
+				}
+			}
+		},
+		disableMarkdownInlineFeatures: {
+			type: Array,
+			default: () => {
+				return []
+			}
+		},
+		disableMarkdownBlockFeatures: {
+			type: Array,
+			default: () => {
+				return []
 			}
 		},
 		autolink: {
@@ -105,46 +112,185 @@ export default {
 			default: false
 		}
 	},
-	render: function(h, context) {
-		// extract text nodes and placeholders and put them into an array that
-		// contains a string for text and an object for each placeholder
-		const placeholders = context.props.text.split(/(\{[a-z\-_.0-9]+\})/ig).map((entry, index, list) => {
-			const matches = entry.match(/^\{([a-z\-_.0-9]+)\}$/i)
-
-			// just return plain string nodes as text
-			if (!matches) {
-				return prepareTextNode({ h, context }, entry)
+	computed: {
+		remarkDisableOptions() {
+			return {
+				inline: [...(this.autolink ? [] : ['url']), ...this.disableMarkdownInlineFeatures],
+				block: this.disableMarkdownBlockFeatures
 			}
+		}
+	},
+	render(h) {
+		const renderedMarkdown = unified()
+			.use(markdown)
+			.use(remarkExternalLinks, {
+				target: '_blank',
+				rel: ['noopener noreferrer']
+			})
+			.use(remarkDisableBlocks, this.remarkDisableOptions)
+			.use(remark2rehype, {
+				handlers: {
+					component(toHast, node) {
+						return toHast(node, node.component, { value: node.value })
+					}
+				}
+			})
+			.use(rehypeAddClasses, this.markdownCssClasses)
+			.use(pluginComponent)
+			.use(rehype2react, {
+				createElement: (tag, attrs, children) => {
+					if (!tag.startsWith('#')) {
+						return h(tag, { attrs }, children)
+					}
 
-			// return component instance if argument is an object
-			const argumentId = matches[1]
-			const argument = context.props.arguments[argumentId]
-			if (typeof argument === 'object') {
-				const { component, props } = argument
-				return h(component, {
-					props,
-					class: 'rich-text--component'
-				})
-			}
+					const placeholder = this.arguments[tag.slice(1)]
+					if (!placeholder) {
+						return h('span', { ...{ attrs }, ...{ class: 'rich-text--fallback' } }, [`{${tag.slice(1)}}`])
+					}
 
-			if (argument) {
-				return h('span', { class: 'rich-text--fallback' }, argument)
-			}
+					if (!placeholder.component) {
+						return h('span', { ...{ attrs } }, [placeholder])
+					}
 
-			return entry
-		})
+					return h(
+						placeholder.component,
+						{
+							attrs,
+							props: placeholder.props,
+							class: 'rich-text--component'
+						},
+						children
+					)
+				},
+				prefix: false
+			})
+			.processSync(this.text)
+			.contents
 
-		return h('div', { class: 'rich-text--wrapper' }, placeholders.flat())
+		return h('div', { class: 'rich-text--wrapper' }, [renderedMarkdown])
 	}
 }
 </script>
 
-<style scoped>
-	.rich-text--fallback, .rich-text-component {
-		display: inline;
+<style lang="scss" scoped>
+.rich-text--wrapper {
+	word-break: break-word;
+	line-height: 1.5;
+}
+
+.rich-text--fallback, .rich-text-component {
+	display: inline;
+}
+
+.rich-text--external-link:after {
+	content: ' ↗';
+}
+
+.rich-text--list-element {
+	list-style: initial;
+	white-space: initial;
+	color: var(--color-text-light);
+	padding: initial;
+	margin-left: 20px;
+
+	li {
+		padding: initial;
 	}
 
-	a.external:after {
-		content: " ↗";
+	&.task-list-item {
+		list-style: none;
+		white-space: initial;
+		color: var(--color-text-light);
+
+		input {
+			min-height: initial;
+		}
 	}
+}
+
+.rich-text--strong {
+	white-space: initial;
+	font-weight: bold;
+	color: var(--color-text-light);
+}
+
+.rich-text--italic {
+	white-space: initial;
+	font-style: italic;
+	color: var(--color-text-light);
+}
+
+.rich-text--heading {
+	white-space: initial;
+	font-size: initial;
+	color: var(--color-text-light);
+	margin-bottom: 5px;
+	margin-top: 5px;
+	font-weight: bold;
+
+	&.rich-text--heading-1 {
+		font-size: 20px;
+	}
+
+	&.rich-text--heading-2 {
+		font-size: 19px;
+	}
+
+	&.rich-text--heading-3 {
+		font-size: 18px;
+	}
+
+	&.rich-text--heading-4 {
+		font-size: 17px;
+	}
+
+	&.rich-text--heading-5 {
+		font-size: 16px;
+	}
+
+	&.rich-text--heading-6 {
+		font-size: 15px;
+	}
+}
+
+.rich-text--hr {
+	border-top: 1px solid var(--color-border-dark);
+	border-bottom: 0;
+}
+
+.rich-text--pre {
+	border: 1px solid var(--color-border-dark);
+	background-color: var(--color-background-dark);
+	padding: 5px;
+}
+
+.rich-text--code {
+	background-color: var(--color-background-dark);
+}
+
+.rich-text--blockquote {
+	border-left: 3px solid var(--color-border-dark);
+	padding-left: 5px;
+}
+
+.rich-text--table {
+	border-collapse: collapse;
+
+	thead tr th {
+		border: 1px solid var(--color-border-dark);
+		font-weight: bold;
+		padding: 6px 13px;
+	}
+
+	tbody tr {
+		td {
+			border: 1px solid var(--color-border-dark);
+			padding: 6px 13px;
+		}
+
+		&:nth-child(even) {
+			background-color: var(--color-background-dark);
+		}
+	}
+}
 </style>
