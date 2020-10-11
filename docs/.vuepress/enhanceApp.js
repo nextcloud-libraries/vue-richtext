69cbd51c92af1cdf6f0ded3bff3b5e481a7a3380
Cacodaimon <mail@cacodaemon.de>
2020-10-12 01:42:03 +0200

    Fixed docs where unable to build, added md example to doc.

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,12 +1,8 @@
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 
-export default ({
-    Vue, // the version of Vue being used in the VuePress app
-    options, // the options for the root Vue instance
-    router, // the router instance for the app
-    siteData // site metadata
-  }) => {
-    const RichText = require('../../src/index.js');
-    Vue.component('RichText', RichText.default);
+import RichText from '../../src/RichText';
+
+export default ({ Vue }) => {
+    Vue.component('RichText', RichText);
   };
---
+++ docs/markdown.md
@@ -0,0 +1,13 @@
+## Renders Markdown
+
+### Example output
+
+<br />
+<RichTextDemoMarkdown />
+<br />
+
+### Example source
+<<< @/docs/.vuepress/components/RichTextDemoMarkdown.vue
+
+#### User.vue
+<<< @/docs/.vuepress/components/User.vue
