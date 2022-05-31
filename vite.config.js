8bea63a4a9835eedd995a9a4900d809b0d77f65e
Julius Härtl <jus@bitgrid.net>
2022-05-31 11:11:45 +0200

    Switch to vite for packaging
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import { createVuePlugin } from 'vite-plugin-vue2'
+import { dependencies } from './package.json'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [createVuePlugin()],
+	build: {
+		lib: {
+			entry: resolve(__dirname, 'src/package.js'),
+			name: '@juliushaertl/vue-richtext',
+			fileName: (_format) => 'index.js',
+			formats: ['es'],
+		},
+		rollupOptions: {
+			external: Object.keys(dependencies),
+			output: {
+				globals: { vue: 'Vue' }
+			},
+		},
+		minify: false,
+	},
+})
