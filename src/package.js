8772a6abb1df98b6d7695c00acf8eae764307617
Julius Härtl <jus@bitgrid.net>
2022-08-12 17:08:41 +0200

    Add axios and export more functions
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- src/package.js
+++ src/package.js
@@ -1,3 +1,16 @@
 import RichText from './RichText.vue'
+import { registerWidget, renderWidget, isWidgetRegistered } from './widgets'
+import References from './References.vue'
+import ReferenceWidget from './ReferenceWidget.vue'
 
 export default RichText
+
+export {
+    RichText,
+    References,
+    ReferenceWidget,
+
+    registerWidget,
+    renderWidget,
+    isWidgetRegistered,
+}
